c6e27113ff8e0726f5052e792b36dc99008e6828
Gayathri <gayathrikilaru13@gmail.com>
2026-06-16 22:34:50 +0530

    creating py file

---
+++ python.py
@@ -0,0 +1,19 @@
+Simple Python program
+Print a message
+print("Hello, Python learner!")
+
+Do some math
+a = 5
+b = 3
+sum_result = a + b
+print("The sum of", a, "and", b, "is:", sum_result)
+
+Use a loop
+for i in range(1, 6):
+    print("Counting:", i)
+
+Define a function
+def greet(name):
+    return "Hello, " + name + "!"
+
+print(greet("Gayathri"))
